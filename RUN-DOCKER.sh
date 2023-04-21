9af678e45c313e88d3da8bc3ae2c6ce2ff8ff087
frrobot-ai <wangyan@frtech.fr>
2023-04-21 17:20:44 +0800

    fix mixin

--- RUN-DOCKER.sh
+++ RUN-DOCKER.sh
@@ -15,7 +15,7 @@ echo "$0: CONTAINER=${CONTAINER}"
 
 # Run the Docker container in the background.
 # Any changes made to './docker/docker-compose.yml' will recreate and overwrite the container.
-docker compose -p ${PROJECT} -f ./docker/docker-compose.yml up -d
+docker-compose -p ${PROJECT} -f ./docker/docker-compose.yml up -d
 
 ################################################################################
 
